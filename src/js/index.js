639830ad976ee824570b53db4840e48998250594
Andrea Betancourt <111385731+AndreaBetan@users.noreply.github.com>
2023-07-07 21:22:29 +0000

    Primera parte

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,17 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+// import Home from "./component/home.jsx";
+import { SecondsCounter } from "./component/SecondsCounter";
+
+let counter = 0;
+
+setInterval(() => {
+    // render your react application
+    ReactDOM.render(<SecondsCounter counter={counter}/>, document.querySelector("#app"));
+    // incremento el contador
+    counter++;
+}, 1000);
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+// ReactDOM.render(<Home />, document.querySelector("#app"));
